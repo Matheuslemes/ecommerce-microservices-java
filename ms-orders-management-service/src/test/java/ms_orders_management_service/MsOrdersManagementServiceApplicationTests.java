fb543338f9e262e87e9479485626d1368b5f4e63
Matheuslemes <matheuslemesmsl@gmail.com>
2024-09-21 16:08:41 -0300

    uploading the project

---
+++ ms-orders-management-service/src/test/java/ms_orders_management_service/MsOrdersManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ms_orders_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrdersManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-payments-processing-service/src/test/java/ms_payments_processing_service/MsPaymentsProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ms_payments_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPaymentsProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
